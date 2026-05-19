a8b181a6e069354851acbc47af14de34dbfab0c7
calebmear <cmmear@gmail.com>
2026-05-18 20:25:39 -0400

    mobile commit v3

--- components/AppSplash.tsx
+++ components/AppSplash.tsx
@@ -1,5 +1,27 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 export default function AppSplash() {
-  return null;
+  const [showSplash, setShowSplash] = useState(false);
+
+  useEffect(() => {
+    const alreadySeen = sessionStorage.getItem('theqIntroSeen');
+
+    if (alreadySeen) return;
+
+    sessionStorage.setItem('theqIntroSeen', 'true');
+    setShowSplash(true);
+  }, []);
+
+  if (!showSplash) return null;
+
+  return (
+    <div
+      className="pointer-events-none fixed inset-0 z-[9999] bg-white"
+      onAnimationEnd={() => setShowSplash(false)}
+    >
+      <div className="theq-intro-mark">Q</div>
+    </div>
+  );
 }
